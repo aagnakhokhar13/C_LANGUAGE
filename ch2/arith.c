68505c7e307dcd07a91af3eafab0f6da0776268e
aagnakhokhar13 <144038977+aagnakhokhar13@users.noreply.github.com>
2023-09-04 19:57:26 +0530

    Add files via upload

---
+++ ch2/arith.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum,sub,multiply,div,mod;
+     a=50;
+     b=10;
+    
+     sum=a+b;
+     sub=a-b;
+     multiply=a*b;
+     div=a/b;
+     mod=a%b;
+
+      printf("additon of %d and %d =%d\n",sum);
+       printf("sub of %d and %d =%d\n",sub);
+        printf("multiply of %d and %d =%d\n",multiply);
+         printf("div of %d and %d =%d\n",div);
+          printf("modulus of %d and %d =%d\n",mod);
+
+}
