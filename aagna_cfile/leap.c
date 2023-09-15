8fe6f16ab0364cd4a020a08c5df3644dab64b910
aagnakhokhar13 <144038977+aagnakhokhar13@users.noreply.github.com>
2023-09-15 19:48:11 +0530

    Add files via upload

---
+++ aagna_cfile/leap.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main(){
+	int x,y;
+	printf("Enter the year :-");
+	scanf("%d",&x);
+	printf("Enter the year :-");
+	scanf("%d",&y);
+	
+	
+	while(x<=y){
+	  
+    printf("%d\n",x);
+    x=x+4;
+	}
+	
+}
---
+++ aagna_cfile/oddno.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main(){
+	int n;
+	printf("Enter the number:-");
+	scanf("%d",&n);
+	
+	while(n>=1){
+		
+		printf("%d  ",n);
+		n=n-2;
+	}
+	
+}
