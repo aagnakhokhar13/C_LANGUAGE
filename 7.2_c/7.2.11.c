a452551ea7c0864de614dc2d6d1b0ca65f9bbb87
aagnakhokhar13 <144038977+aagnakhokhar13@users.noreply.github.com>
2023-09-24 17:07:53 +0530

    Add files via upload

---
+++ 7.2_c/7.2.11.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main(){
+    int i,j,s,n;
+    printf("Enter the number ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(s=i;s<=n-1;s++)
+        {
+        printf("-");
+        }
+        for(j=1;j<=i;j++){
+            if(j%2==0)
+            printf("*");
+            else
+                printf(" ");
+          printf("* ");
+        }
+        printf("\n");
+    }
+}
