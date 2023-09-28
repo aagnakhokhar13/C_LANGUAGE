9b85ca52294164d3077160ab96b9acb41a3b9d11
aagnakhokhar13 <144038977+aagnakhokhar13@users.noreply.github.com>
2023-09-28 10:37:52 +0530

    Add files via upload

--- 8.2.c/8.2.1.c
+++ 8.2.c/8.2.1.c
@@ -1,12 +1,32 @@
-#include<stdio.h>
-main(){
-    int n;
-    int a[n];
 
-    printf("Enter the array element :-");
-    scanf(" %d",&n);
-    for(i=0;i<n;i++)
-    {
-      
+
+#include<stdio.h>
+void main(){
+    int n,m;
+    printf("Enter the size of array :-");
+    scanf("%d",&n);
+  printf("Enter the array size:-");
+        scanf("%d",&m);
+    int a[n];
+    int b[m];
+    int c[n+m];
+    int i;
+    for(i=0;i<n;i++){
+        printf("Enter a[%d]",i);
+        scanf("%d",&a[i]);
+        c[i]=a[i];
     }
+      
+    for(i=0;i<m;i++){
+        printf("Enter b[%d]",i);
+        scanf("%d",&b[i]);
+        c[i+n]=b[i];
+        
+    }
+ 
+    printf("Sum of a and b are below:- \n");
+     for(i=0;i<n+m;i++){
+        printf("%d ",c[i]);
+     }
+return;
 }
