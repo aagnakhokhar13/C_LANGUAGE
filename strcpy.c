a4ccc0e82d41307b2b43f17a67c9af19c773549f
aagnakhokhar13 <144038977+aagnakhokhar13@users.noreply.github.com>
2023-10-05 19:40:29 +0530

    Delete strcpy.c

--- strcpy.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<string.h>
-void main(){
-	char ch1[20] , ch2[20];
-	printf("Enter the string :-");
-	gets(ch1);
-	printf("Ans = %s\n",strcpy(ch2,ch1));
-	printf("copy ch2 = %s",ch2);
-}
