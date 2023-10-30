3897368f068899f6abee128310c6359f5d5f4c26
aagnakhokhar13 <144038977+aagnakhokhar13@users.noreply.github.com>
2023-10-30 19:25:37 +0530

    Add files via upload

---
+++ exam_c/interest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void interest(){
+
+
+interest();
+}
+void main(){
+	int p,r,t,interest;
+printf("Enter the principal:-");
+scanf("%d",&p);
+printf("Enter the rate:-");
+scanf("%d",&r);
+printf("Enter the time:-");
+scanf("%d",&t);
+
+interest=p*r*t/100;
+}
+
