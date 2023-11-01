0ffed6027e785af310387efa8e9378bcbab995d7
aagnakhokhar13 <144038977+aagnakhokhar13@users.noreply.github.com>
2023-11-01 19:46:21 +0530

    Update prog3.c

--- reexam_c/prog3.c
+++ reexam_c/prog3.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 main(){
-    int arr[40];
-    int i,min,,n,max;
+    int a[40];
+    int i,min,n,max;
    
     printf("Enter the array size");
     scanf("%d",&n);
     printf("Enter the array element:-");
-    for(i=0;i<n;i++){
+    for(i=0;i<n;i++)
+    {
         scanf("%d",&a[i]);
     }
     min=max=a[0];
-    for(i=0;i<n;i++){
+    for(i=1;i<n;i++){
         if(min>a[i])
         min=a[i];
         if(max<a[i])
         max=a[i];
 }
-printf("minimum of array is :%d",min);
+printf("\nminimum of array is :%d",min);
 printf("maximum of array is= %d",max);
 }
